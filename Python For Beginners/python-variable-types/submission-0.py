4de246bc7454805f79b376368a7a00fcf41ec683
Adi0SS <145386542+Adi0SS@users.noreply.github.com>
2026-07-11 15:23:03 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,15 @@
+integer_type = 5
+float_type = 6.6
+bool_type = True
+string_type = "hello mf"
+list_type= [5,4442,444.55, "hello"]
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
